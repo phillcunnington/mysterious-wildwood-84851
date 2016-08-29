9bdfe1d34c1139a2213d14220f5861923529f936
Phill Cunnington <phill.cunnington@forgerock.com>
2016-08-29 21:54:34 +0100

    Add failed login page

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Jumbotron } from "react-bootstrap";
+import { Jumbotron, Button } from "react-bootstrap";
 import { Router, Route, browserHistory } from "react-router";
+import { LinkContainer } from "react-router-bootstrap";
 
 class LandingPage extends React.Component {
   render() {
@@ -10,6 +11,23 @@ class LandingPage extends React.Component {
         <Jumbotron>
           <h1>Welcome!</h1>
           <p>This is the accounting, budgeting app.</p>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
+
+class FailedLoginPage extends React.Component {
+  render() {
+    return (
+      <div style={{textAlign: "center"}}>
+        <Jumbotron>
+          <p>Login Failed!</p>
+          <p>
+            <LinkContainer to="/">
+              <Button bsStyle="primary" href="/">Back</Button>
+            </LinkContainer>
+          </p>
         </Jumbotron>
       </div>
     );
@@ -30,6 +48,7 @@ ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={LandingPage} />
     <Route path="/app" component={App} />
+    <Route path="/loginFailed" component={FailedLoginPage} />
   </Router>,
     document.getElementById("app")
 );
--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 const logger = require("morgan");
 
+const fallback = require("express-history-api-fallback");
+const root = `${__dirname}/../public`;
 
 app.use(logger(process.env.HTTP_LOGGING_FORMAT));
 
-
-
 app.use(express.static("public"));
 
+app.use(fallback("index.html", { root }));
 
 app.listen(port);
