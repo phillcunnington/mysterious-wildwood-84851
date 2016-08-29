840e8ec82845d663053596cb1309c8e07ae23299
Phill Cunnington <phill.cunnington@forgerock.com>
2016-08-29 21:40:00 +0100

    Add routing to UI

--- app/index.js
+++ app/index.js
@@ -1,21 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Jumbotron} from "react-bootstrap";
+import { Jumbotron } from "react-bootstrap";
+import { Router, Route, browserHistory } from "react-router";
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <div style={{textAlign: "center"}}>
+        <Jumbotron>
+          <h1>Welcome!</h1>
+          <p>This is the accounting, budgeting app.</p>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
 
 class App extends React.Component {
   render() {
     return (
-        <div style={{textAlign: "center"}}>
-          <Jumbotron>
-            <h1>Welcome!</h1>
-            <p>This is the accounting, budgeting app.</p>
-          </Jumbotron>
-        </div>
+      <div>
+          This is the protected app!
+      </div>
     );
   }
 }
 
 ReactDOM.render(
-  <App />,
-  document.getElementById("app")
+  <Router history={browserHistory}>
+    <Route path="/" component={LandingPage} />
+    <Route path="/app" component={App} />
+  </Router>,
+    document.getElementById("app")
 );
