2137b7fb3a3fa95b61742405ea516d7402e20195
Phill Cunnington <phill.cunnington@forgerock.com>
2016-09-09 17:32:49 +0100

    Add transaction list view

--- app/App.js
+++ app/App.js
@@ -1,12 +1,35 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import TransactionListView from "./TransactionListView";
+import _ from "lodash";
 
 export default React.createClass({
+  getInitialState: function() {
+      return {
+          transactions: []
+      }
+  },
+
+  componentDidMount: function() {
+    fetch("/api/transactions", {
+      method: "GET",
+      credentials: "same-origin"
+    }).then(response => {
+      return response.json();
+    }).then((json) => {
+      this.setState({
+        transactions: _.orderBy(json, ["date"], ["desc"])
+      });
+    }).catch(error => {
+      console.error("Failed to get transactions");
+    });
+  },
+
   render() {
     return (
-      <div>
-        This is the protected app!
-        <span><Button bsStyle="link" href="auth/logout">Logout</Button></span>
+      <div className="app">
+        <span className="app__logout-button"><Button bsStyle="link" href="auth/logout">Logout</Button></span>
+        <TransactionListView transactions={this.state.transactions} />
       </div>
     );
   }
---
+++ app/TransactionListView.js
@@ -0,0 +1,78 @@
+import React, { PropTypes } from "react";
+import { ListGroup } from "react-bootstrap";
+import _ from "lodash";
+import moment from "moment";
+import numeral from "numeral";
+numeral.defaultFormat("$0,0.00");
+numeral.language('en-GB', {
+  delimiters: {
+    thousands: ',',
+    decimal: '.'
+  },
+  currency: {
+    symbol: '£'
+  }
+});
+numeral.language('en-GB');
+
+const groupTransactionsByDate = (transactions) => {
+  return _.reduce(transactions, (result, transaction) => {
+    if (!result[transaction.date]) {
+      result[transaction.date] = [];
+    }
+    result[transaction.date].push(_.omit(transaction, "date"));
+    return result;
+  }, {});
+};
+
+const generateListItems = (transactions) => {
+  return _.reduce(groupTransactionsByDate(transactions), (result, transactionGroup, date) => {
+    result.push(<TransactionListItemDateHeading key={date} date={date}/>);
+    return result.concat(_.map(transactionGroup, (transaction) => {
+      return <TransactionListItem key={transaction._id} transaction={transaction}/>
+    }));
+  }, []);
+};
+
+const TransactionListItemDateHeading = ({ date }) => (
+    <li className="list-group-item disabled">
+      {moment(date).format("dddd, D MMMM")}
+    </li>
+);
+TransactionListItemDateHeading.propTypes = {
+  date: PropTypes.string.isRequired
+};
+
+const TransactionListItem = ({ transaction }) => (
+    <li className="list-group-item">
+    <span>
+      {transaction.payee}
+    </span>
+      <span className="transaction-list__list-item-amount">
+      {numeral(transaction.amount / 100).format()}
+    </span>
+    </li>
+);
+TransactionListItem.propTypes = {
+  transaction: PropTypes.shape({
+    payee: PropTypes.string.isRequired,
+    amount: PropTypes.number.isRequired
+  }).isRequired
+};
+
+const TransactionList = ({ transactions }) => (
+  <div className="transaction-list">
+    <ListGroup componentClass="ul">
+      {generateListItems(transactions)}
+    </ListGroup>
+  </div>
+);
+TransactionList.propTypes = {
+  transaction: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    payee: PropTypes.string.isRequired,
+    amount: PropTypes.number.isRequired
+  })
+};
+
+export default TransactionList
