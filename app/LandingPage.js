54d4057b1038c5632d9bca2bff5c62f93594948d
Phill Cunnington <phill.cunnington@forgerock.com>
2016-09-09 17:31:50 +0100

    Convert stateless views to functional views

--- app/LandingPage.js
+++ app/LandingPage.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { Jumbotron, Button } from "react-bootstrap";
 
-export default React.createClass({
-  render() {
-    return (
-      <div style={{textAlign: "center"}}>
-        <Jumbotron>
-          <h1>Welcome!</h1>
-          <p>This is the accounting, budgeting app.</p>
-          <p><Button bsStyle="primary" href="auth/google">Login</Button></p>
-        </Jumbotron>
-      </div>
-    );
-  }
-})
+const LandingPage = () => (
+  <div style={{textAlign: "center"}}>
+    <Jumbotron>
+      <h1>Welcome!</h1>
+      <p>This is the accounting, budgeting app.</p>
+      <p><Button bsStyle="primary" href="auth/google">Login</Button></p>
+    </Jumbotron>
+  </div>
+);
+
+export default LandingPage
